e90dff98e688ed86f4e356fa6448a40be245048d
Katie Fritz <katie@katiemfritz.com>
2017-04-16 09:07:21 -0400

    initialize firebase and sign in anonymously

--- woofer-db.js
+++ woofer-db.js
@@ -1,5 +1,19 @@
 // TODO Sign into the database anonymously
 
+// Initialize Firebase
+var config = {
+  apiKey: 'AIzaSyA8pXiNlI_51_iEcKEBc0NSsVXOjKjysYI',
+  authDomain: 'woofer-7b430.firebaseapp.com',
+  databaseURL: 'https://woofer-7b430.firebaseio.com',
+  projectId: 'woofer-7b430',
+  storageBucket: 'woofer-7b430.appspot.com',
+  messagingSenderId: '76059962967'
+}
+firebase.initializeApp(config)
+
+firebase.auth().signInAnonymously()
+
+
 // CREATE a new woof in Firebase
 function createWoofInDatabase (woof) {
   // TODO create a new record in Firebase
