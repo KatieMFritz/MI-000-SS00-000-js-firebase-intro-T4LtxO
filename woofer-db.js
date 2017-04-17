5eb901f741b37f72770f0823f07927e928e3248f
Katie Fritz <katie@katiemfritz.com>
2017-04-17 13:57:49 -0400

    update updateWoofInDatabase

--- woofer-db.js
+++ woofer-db.js
@@ -39,9 +39,7 @@ function readWoofsInDatabase () {
 
 // UPDATE the woof in Firebase
 function updateWoofInDatabase (woofKey, woofText) {
-  firebase.database().ref('woofs').child(woofKey).set({
-    text: woofText
-  })
+  firebase.database().ref('woofs').child(woofKey).child('text').set(woofText)
 }
 
 // DELETE the woof from Firebase
